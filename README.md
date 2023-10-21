9e07656fb505f4c3e7bcfc2728049d8c179a2942
Quentin Guidée <quentin.guidee@gmail.com>
2023-10-21 11:48:45 -0400

    feat: initialize jetbrains-icons with Gradle

---
+++ README.md
@@ -0,0 +1,36 @@
+<h3 align="center">
+	<img src="https://raw.githubusercontent.com/catppuccin/catppuccin/main/assets/logos/exports/1544x1544_circle.png" width="100" alt="Logo"/><br/>
+	<img src="https://raw.githubusercontent.com/catppuccin/catppuccin/main/assets/misc/transparent.png" height="30" width="0px"/>
+	Catppuccin icons for <a href="https://www.jetbrains.com">JetBrains IDEs</a>
+	<img src="https://raw.githubusercontent.com/catppuccin/catppuccin/main/assets/misc/transparent.png" height="30" width="0px"/>
+</h3>
+
+<p align="center">
+	<a href="https://github.com/catppuccin/jetbrains-icons/stargazers"><img src="https://img.shields.io/github/stars/catppuccin/jetbrains-icons?colorA=363a4f&colorB=b7bdf8&style=for-the-badge"></a>
+	<a href="https://github.com/catppuccin/jetbrains-icons/issues"><img src="https://img.shields.io/github/issues/catppuccin/jetbrains-icons?colorA=363a4f&colorB=f5a97f&style=for-the-badge"></a>
+	<a href="https://github.com/catppuccin/jetbrains-icons/contributors"><img src="https://img.shields.io/github/contributors/catppuccin/jetbrains-icons?colorA=363a4f&colorB=a6da95&style=for-the-badge"></a>
+</p>
+
+<!-- this section is optional -->
+## 🙋 FAQ
+
+-	Q: **_"Where can I install?"_**\
+  A: This theme is not ready yet!
+
+## 💝 Thanks to
+
+- [Quentin](https://github.com/quentinguidee)
+
+&nbsp;
+
+<p align="center">
+	<img src="https://raw.githubusercontent.com/catppuccin/catppuccin/main/assets/footers/gray0_ctp_on_line.svg?sanitize=true" />
+</p>
+
+<p align="center">
+	Copyright &copy; 2021-present <a href="https://github.com/catppuccin" target="_blank">Catppuccin Org</a>
+</p>
+
+<p align="center">
+	<a href="https://github.com/catppuccin/catppuccin/blob/main/LICENSE"><img src="https://img.shields.io/static/v1.svg?style=for-the-badge&label=License&message=MIT&logoColor=d9e0ee&colorA=363a4f&colorB=b7bdf8"/></a>
+</p>
---
+++ build.gradle.kts
@@ -0,0 +1,87 @@
+import org.jetbrains.changelog.Changelog
+import org.jetbrains.changelog.date
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+fun properties(key: String) = project.findProperty(key).toString()
+
+plugins {
+    // Java support
+    id("java")
+    // Kotlin support
+    id("org.jetbrains.kotlin.jvm") version "1.6.10"
+    // Gradle IntelliJ Plugin
+    id("org.jetbrains.intellij") version "1.16.0"
+    // Gradle Changelog Plugin
+    id("org.jetbrains.changelog") version "2.0.0"
+}
+
+group = properties("pluginGroup")
+version = properties("pluginVersion")
+
+repositories {
+    mavenCentral()
+}
+
+intellij {
+    pluginName.set(properties("pluginName"))
+    version.set(properties("platformVersion"))
+    type.set(properties("platformType"))
+
+    // Plugin Dependencies. Uses `platformPlugins` property from the gradle.properties file.
+    plugins.set(properties("platformPlugins").split(',').map(String::trim).filter(String::isNotEmpty))
+}
+
+changelog {
+    version.set(properties("pluginVersion"))
+    path.set(file("CHANGELOG.md").canonicalPath)
+    header.set(provider { "${version.get()} - ${date()}" })
+    headerParserRegex.set("""(\d\.\d\.\d)""".toRegex())
+    itemPrefix.set("-")
+    keepUnreleasedSection.set(true)
+    unreleasedTerm.set("[Unreleased]")
+    groups.set(listOf("Added", "Changed", "Deprecated", "Removed", "Fixed", "Security"))
+}
+
+tasks {
+    // Set the JVM compatibility versions
+    properties("javaVersion").let {
+        withType<JavaCompile> {
+            sourceCompatibility = it
+            targetCompatibility = it
+        }
+        withType<KotlinCompile> {
+            kotlinOptions.jvmTarget = it
+        }
+    }
+
+    wrapper {
+        gradleVersion = properties("gradleVersion")
+    }
+
+    patchPluginXml {
+        version.set(properties("pluginVersion"))
+        sinceBuild.set(properties("pluginSinceBuild"))
+        untilBuild.set(properties("pluginUntilBuild"))
+
+        // Get the latest available change notes from the changelog file
+        changeNotes.set(provider {
+            changelog.renderItem(changelog.getLatest(), Changelog.OutputType.HTML)
+        })
+    }
+
+    signPlugin {
+        certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
+        privateKey.set(System.getenv("PRIVATE_KEY"))
+        password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
+    }
+
+    publishPlugin {
+        dependsOn("patchChangelog")
+        token.set(System.getenv("PUBLISH_TOKEN"))
+        channels.set(listOf("default"))
+    }
+}
+
+tasks.buildSearchableOptions {
+    enabled = false
+}
