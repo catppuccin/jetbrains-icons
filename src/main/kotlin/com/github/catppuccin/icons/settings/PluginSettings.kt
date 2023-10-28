668b5676cfae6e1e088f30c811cadd2b5b85b335
Quentin Guidée <quentin.guidee@gmail.com>
2023-10-28 17:36:59 -0400

    feat: restart popup on settings change (#1)
    Signed-off-by: Quentin Guidée <quentin.guidee@gmail.com>

--- src/main/kotlin/com/github/catppuccin/icons/settings/PluginSettings.kt
+++ src/main/kotlin/com/github/catppuccin/icons/settings/PluginSettings.kt
@@ -1,7 +1,16 @@
 package com.github.catppuccin.icons.settings
 
+import com.intellij.ide.GeneralSettings
+import com.intellij.ide.IdeBundle
+import com.intellij.notification.NotificationGroupManager
+import com.intellij.openapi.actionSystem.ActionManager
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.application.ex.ApplicationEx
 import com.intellij.openapi.options.Configurable
+import com.intellij.openapi.ui.Messages
 import javax.swing.JComponent
+
 
 class PluginSettings : Configurable {
     private val panel = PluginSettingsPanel(PluginSettingsState.instance.variant)
@@ -18,9 +27,29 @@ class PluginSettings : Configurable {
     override fun apply() {
         val state = PluginSettingsState.instance
         state.variant = panel.variant
+        restart()
     }
 
     override fun getDisplayName(): String {
         return "Catppuccin Icons"
+    }
+
+    private fun restart() {
+        val result = if (GeneralSettings.getInstance().isConfirmExit) {
+            Messages.showYesNoDialog(
+                "The IDE needs to be restarted for the changes to take effect. Restart now?",
+                IdeBundle.message("dialog.title.restart.ide"),
+                IdeBundle.message("dialog.action.restart.yes"),
+                IdeBundle.message("dialog.action.restart.cancel"),
+                Messages.getWarningIcon()
+            ) == Messages.YES
+        } else {
+            true
+        }
+
+        if (result) {
+            val app = ApplicationManager.getApplication() as ApplicationEx
+            app.restart(true)
+        }
     }
 }
