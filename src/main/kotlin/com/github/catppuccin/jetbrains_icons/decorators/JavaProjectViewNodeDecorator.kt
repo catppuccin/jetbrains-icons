2a52f80537e17507e1bd9ef4909e8b94c3f5dfd4
thelooter <evekolb2204@gmail.com>
2024-05-16 20:48:33 +0200

    feat(icons): Add Support for different Icons for Different Java Class Types
    Signed-off-by: thelooter <evekolb2204@gmail.com>

--- src/main/kotlin/com/github/catppuccin/jetbrains_icons/decorators/JavaProjectViewNodeDecorator.kt
+++ src/main/kotlin/com/github/catppuccin/jetbrains_icons/decorators/JavaProjectViewNodeDecorator.kt
@@ -1,22 +1,31 @@
 package com.github.catppuccin.jetbrains_icons.decorators
 
+import com.github.catppuccin.jetbrains_icons.IconPack
+import com.github.catppuccin.jetbrains_icons.util.PsiClassUtils
 import com.intellij.ide.projectView.PresentationData
 import com.intellij.ide.projectView.ProjectViewNode
 import com.intellij.ide.projectView.ProjectViewNodeDecorator
+import com.intellij.lang.jvm.JvmModifier
+import com.intellij.psi.PsiClass
+import org.jetbrains.kotlin.idea.codeInsight.SuperDeclaration
 import org.jetbrains.kotlin.psi.*
+import org.jetbrains.kotlin.psi.psiUtil.isAbstract
+import org.jetbrains.kotlin.psi.psiUtil.isObjectLiteral
 
 class JavaProjectViewNodeDecorator : ProjectViewNodeDecorator {
     override fun decorate(node: ProjectViewNode<*>, data: PresentationData) {
         if (node.virtualFile?.isDirectory == true) return
 
-        val value = node.value
-        when (value) {
-            is KtClass -> when {
-                // value.isInterface() ->
-                // value.isEnum() ->
-                // value.isAnnotation() ->
-                // value.isSealed() ->
-                // else ->
+        val icons = IconPack.instance.icons
+
+        when (val value = node.value) {
+
+            is PsiClass -> when {
+                value.isInterface -> data.setIcon(icons.java_alt_1)
+                value.isAnnotationType -> data.setIcon(icons.java_alt_1)
+                value.isEnum -> data.setIcon(icons.java_alt_3)
+                value.isRecord -> data.setIcon(icons.java_alt_2)
+                PsiClassUtils.isAbstract(value) -> data.setIcon(icons.java_alt_1)
             }
         }
     }
