ba47af4befd0eb01b6f278de333fa2281f719ccb
Kaley Main <kaleymain@google.com>
2024-11-04 13:46:27 +1100

    chore: add tests for IconProvider and PsiClassUtils

--- src/main/kotlin/com/github/catppuccin/jetbrains_icons/util/PsiClassUtils.kt
+++ src/main/kotlin/com/github/catppuccin/jetbrains_icons/util/PsiClassUtils.kt
@@ -4,6 +4,9 @@ import com.intellij.psi.PsiClass
 import com.intellij.psi.PsiClassType
 import com.intellij.psi.PsiModifier
 
+/**
+ * Utilities for determining properties about a [PsiClass] in order to correctly display an icon.
+ */
 object PsiClassUtils {
   fun isAbstract(psiClass: PsiClass): Boolean =
     psiClass.isValid && psiClass.hasModifierProperty(PsiModifier.ABSTRACT)
@@ -17,6 +20,7 @@ object PsiClassUtils {
   fun isStatic(psiClass: PsiClass): Boolean =
     psiClass.isValid && psiClass.hasModifierProperty(PsiModifier.STATIC)
 
+  /** Returns true if the [psiClass] is an exception (inherits from an exception). */
   fun isException(psiClass: PsiClass): Boolean {
     val className = psiClass.name
     if (className.isNullOrEmpty()) return false
@@ -24,6 +28,7 @@ object PsiClassUtils {
     return extendsException(psiClass)
   }
 
+  /** Returns true if the [psiClass] has package-private visibility. */
   fun isPackagePrivate(psiClass: PsiClass): Boolean {
     val modifierList = psiClass.modifierList
     return modifierList != null &&
