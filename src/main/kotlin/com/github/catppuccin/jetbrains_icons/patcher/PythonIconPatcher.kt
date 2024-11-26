1addca620d8dbd5705d759c73861011dcbd2cdaa
thelooter <evekolb2204@gmail.com>
2024-11-26 14:56:10 +0100

    fix(python): add mutex to patcher installation

--- src/main/kotlin/com/github/catppuccin/jetbrains_icons/patcher/PythonIconPatcher.kt
+++ src/main/kotlin/com/github/catppuccin/jetbrains_icons/patcher/PythonIconPatcher.kt
@@ -3,16 +3,20 @@ package com.github.catppuccin.jetbrains_icons.patcher
 import com.github.catppuccin.jetbrains_icons.settings.PluginSettingsState
 import com.intellij.openapi.util.IconLoader
 import com.intellij.openapi.util.IconPathPatcher
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 
 class PythonIconPatcher : IconPathPatcher() {
 
   private val settingsInstance = PluginSettingsState.instance
 
   // Only patch the path if Python support is enabled
-  override fun patchPath(path: String, classLoader: ClassLoader?): String? {
-    if (!settingsInstance.pythonSupport || !overrideMap.containsKey(path)) return null
-    return "/jetbrains_icons/icons/${settingsInstance.variant}${overrideMap[path]}"
-  }
+  override fun patchPath(path: String, classLoader: ClassLoader?): String? =
+    if (settingsInstance.pythonSupport && overrideMap.containsKey(path)) {
+      "/jetbrains_icons/icons/${settingsInstance.variant}${overrideMap[path]}"
+    } else {
+      null
+    }
 
   override fun getContextClassLoader(
     path: String,
@@ -20,16 +24,23 @@ class PythonIconPatcher : IconPathPatcher() {
   ): ClassLoader? = javaClass.classLoader
 
   companion object {
-    fun install() = patcherInstance
+    private val isInstalledMutex = Mutex()
+    private var isInstalled = false
 
-    private val patcherInstance: Unit by lazy { IconLoader.installPathPatcher(PythonIconPatcher()) }
+    suspend fun install() =
+      isInstalledMutex.withLock {
+        if (!isInstalled) {
+          isInstalled = true
+          IconLoader.installPathPatcher(PythonIconPatcher())
+        }
+      }
 
-    val overrideMap: Map<String, String> =
+    private val overrideMap: Map<String, String> =
       mapOf(
         // Icon in New UI
         "/icons/com/jetbrains/python/parser/pythonFile.svg" to "_python.svg",
         // Unknown, but reported by UI Inspector when inspecting icon before replacement in new UI
-        "/icons/com/jetbrains/python/parser/expui/python.svg" to "}_python.svg",
+        "/icons/com/jetbrains/python/parser/expui/python.svg" to "_python.svg",
         // Icon in Old UI
         "/icons/com/jetbrains/python/pythonFile.svg" to "_python.svg",
       )
