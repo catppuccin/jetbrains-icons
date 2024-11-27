a701fea5473b5287806c7cd45632b3d46e246293
thelooter <evekolb2204@gmail.com>
2024-11-27 11:44:37 +0100

    fix(python): implement python icon patching (#152)

---
+++ src/main/kotlin/com/github/catppuccin/jetbrains_icons/activity/IconPathPatcherActivity.kt
@@ -0,0 +1,13 @@
+package com.github.catppuccin.jetbrains_icons.activity
+
+import com.github.catppuccin.jetbrains_icons.patcher.PythonIconPatcher
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.startup.ProjectActivity
+import com.intellij.openapi.util.IconPathPatcher
+
+/** Activity responsible for loading and managing [IconPathPatcher] instances */
+class IconPathPatcherActivity : ProjectActivity {
+  override suspend fun execute(project: Project) {
+    PythonIconPatcher.install()
+  }
+}
